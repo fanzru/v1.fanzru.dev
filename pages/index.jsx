a1a2378a29d8e59e00948d71b62aa7dd9e2042a4
Fanzru <fattahilaaf080701@gmail.com>
2021-12-03 18:50:14 +0700

    test meta data

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,16 @@ export default function Home(props) {
       <Head>
         <title>Hello i'm Affan</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta property="og:url" content="your url" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="your fb id" />
+        <meta property="og:title" content={"Hallo"} />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="Hurray!! Yes Social Media Preview is Working"
+        />
+        <meta property="og:image" content={"https://4.bp.blogspot.com/-dlYC3oRgipc/VXKAJ8277lI/AAAAAAAAGxw/PSqW-Jf3LQQ/s1600/Assassination-Classroom-111.jpg"} />
       </Head>
       <div data-theme={"dark"}>
         <Maintenance/>
