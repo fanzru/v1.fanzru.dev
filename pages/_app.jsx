7eb5f4ce6ed8f4f9d209ea5f7cc14a6450f5cf40
fanzru <fattahilaaf080701@gmail.com>
2022-01-18 21:49:42 +0700

    update title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <title>Hello i'm Affan</title>
+        <title>Fanzru</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <meta property="og:url" content="fanzru" />
         <meta property="og:type" content="website" />
